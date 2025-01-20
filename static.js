8b4fed4ce6e952b4f1e8f882370f13aacd8b6bd5
Muhammadsaadazeem <160098312+Muhammadsaadazeem@users.noreply.github.com>
2025-01-20 13:12:22 +0500

    Create static.js

---
+++ static.js
@@ -0,0 +1,12 @@
+function submitForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+
+    if (name === "" || email === "") {
+        alert("Please fill in all fields.");
+    } else {
+        alert("Form submitted successfully!");
+        // Here, you can implement form submission logic like sending to a server
+        document.getElementById("contactForm").reset();
+   }
+}
